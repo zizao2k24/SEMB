c734b08f0965ac656d75e07f9d2155866a47006a
zizao2k24 <00001132668463sp@al.educacao.sp.gov.br>
2026-08-17 16:07:26 -0300

    Add files via upload
    função, calculadora de nota

---
+++ main (17).c
@@ -0,0 +1,28 @@
+/**/
+#include <stdio.h>
+float M, A, B;
+float number = 3.14;
+
+void calcula(void)
+{
+    M = (A + B)/2; 
+}
+
+int main()
+{
+  while (1)
+  {
+      printf("\n\nPrograma de soma. \n");
+      printf("entre com o primeiro numero: ");
+      scanf("%f", &A);
+      printf("entre com o segundo numero: ");
+      scanf("%f", &B);
+     calcula();
+      printf("sua média é: %.2f", M);
+      if (M < 50) printf("você foi reprovado.\n\n");
+      else printf("que arraso, você passou. \n\n");
+      
+      
+     
+  }
+}
